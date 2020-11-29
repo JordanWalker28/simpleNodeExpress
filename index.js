341e2a98b627116f2412e8950a2f8880e4e29d60
Jordan Walker <jordanswalker28@gmail.com>
2020-11-29 15:55:20 +0000

    Moved hogan-Middleware into it's own constant for readability

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const path = require('path')
+const hoganMiddleware = require('hogan-middleware').__express
 
 const app = express()
 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'mustache')
-app.engine('mustache', require('hogan-middleware').__express)
+app.engine('mustache', hoganMiddleware)
  
 app.get('/',  (req,res,next) => {
     res.send("Hello Jordan!")
